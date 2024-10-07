4f803581454843228f23671759efe19554e66857
carlosDAM2905 <carlosdam2905@gmail.com>
2024-10-07 03:00:56 +0200

    video 5 curso DevExpert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.compose.v270)
+
+
+
+
 }
--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.mymovies
 
+import android.media.Image
 import android.media.browse.MediaBrowser.MediaItem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -16,6 +18,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.PlayCircle
+import androidx.compose.material.icons.filled.PlayCircleOutline
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -24,9 +34,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.traceEventEnd
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
@@ -35,6 +48,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.example.mymovies.ui.theme.MyMoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,7 +62,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyMoviesTheme {
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    ButtonText()
+                    MediaItem()
                 }
             }
         }
@@ -54,39 +72,58 @@ class MainActivity : ComponentActivity() {
 //@Preview(showBackground = true, widthDp = 400, heightDp = 100)
 @Composable
 fun ButtonText() {
-    Box(modifier = Modifier.fillMaxSize(),
+    Box(
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Text(
             text = "Hello World",
-            style = MaterialTheme.typography.bodyLarge.copy(shadow = Shadow(
-                offset = Offset(5f, 5f),
-                blurRadius = 5f,
-                color = Color.Black.copy(alpha = 0.5f)
+            style = MaterialTheme.typography.bodyLarge.copy(
+                shadow = Shadow(
+                    offset = Offset(5f, 5f),
+                    blurRadius = 5f,
+                    color = Color.Black.copy(alpha = 0.5f)
 
+                )
             )
-            )
-
         )
     }
 
 }
 
 
-
 @Preview(showBackground = true)
 @Composable
 fun MediaItem() {
-    Column {
+    Column (modifier = Modifier.padding(top = 20.dp)){
         Box(
             modifier = Modifier
                 .height(200.dp)
                 .fillMaxWidth()
-                .background(Color.Red)
-
         ) {
+            AsyncImage(
+                model = "https://loremflickr.com/400/400/cat?lock=1",
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+
+            )
+
+            Icon(
+                imageVector = Icons.Default.PlayCircleOutline,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(88.dp)
+                    .align(Alignment.Center),
+                tint = Color.White
+            )
+
 
         }
+
+
+
+
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
